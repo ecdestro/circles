c55c7bb74d10d44e39f09bc54b44dba025839d9b
ecdestro <61299394+ecdestro@users.noreply.github.com>
2021-06-27 20:21:41 -0500

    Update circles.py

--- circles.py
+++ circles.py
@@ -4,5 +4,5 @@ from math import pi
 rad = float(sys.argv[1])
 print("Circumference: ", '%.5f'%(2 * pi * rad))
 print("Area: ", '%.5f'%(pi * (rad * rad)))
-print("Sphere surface area: ", '%.5f'%(4 * pi * (rad * rad)))
-print("Sphere volume: ", '%.5f'%((4 / 3) * pi * (rad * rad * rad)))
+print("Sphere surface area: ", '%.5f'%(4 * pi * (rad ** 2)))
+print("Sphere volume: ", '%.5f'%((4 / 3) * pi * (rad ** 3)))
